90d43808a5f6de32dd0fbfa9073731fb5c35a1ea
Khanh <21520982@ms.uit.edu.vn>
2025-03-25 03:53:31 +0700

    Initial Commit

---
+++ MedLab/MedLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedLabApp.swift
+//  MedLab
+//
+//  Created by Khanh Nguyen Quoc on 25/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MedLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedLabTests/MedLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedLabTests.swift
+//  MedLabTests
+//
+//  Created by Khanh Nguyen Quoc on 25/3/25.
+//
+
+import Testing
+@testable import MedLab
+
+struct MedLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
